4bb3aff867d2cc63139d8ab5e868c5f8865e0290
harami <harami@example.com>
2020-08-26 12:14:17 +0900

    double state

--- src/App.js
+++ src/App.js
@@ -1,31 +1,28 @@
 import React, { useState } from 'react';
 
-const App = () => {
-  const [count, setCount] = useState(0)
+const App = props => {
+  const [price, setPrice] = useState(props.price)
+  const [name, setName] = useState(props.name)
 
-  const increment = () => { setCount(count + 1) }
-  const decrement = () => { setCount(count - 1) }
-
-  const reset = () => { setCount(0) }
-  const x2 = () => { setCount(previous => previous * 2) }
-  const sprit3 = () => {
-    if (count % 3 === 0) {
-      setCount(count / 3)
-    }
+  const reset = () =>{
+    setPrice(props.price)
+    setName(props.name)
   }
 
   return (
     <React.Fragment>
-      <div>count: {count}</div>
-      <div>
-        <button onClick={increment}>+1</button>
-        <button onClick={decrement}>-1</button>
-      </div>
+      <div>現在の{name}は{price}円です。</div>
+      <button onClick={() => setPrice(price + 1)}>+1</button>
+      <button onClick={() => setPrice(price - 1)}>-1</button>
       <button onClick={reset}>reset</button>
-      <button onClick={x2}>x2</button>
-      <button onClick={sprit3}>3の倍数の時だけ３で割る</button>
+      <input value={name} onChange={e => setName(e.target.value)}/>
     </React.Fragment>
   )
+}
+
+App.defaultProps = {
+  name:"sample",
+  price:1000
 }
 
 export default App
